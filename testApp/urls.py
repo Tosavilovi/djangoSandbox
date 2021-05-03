2970d74f0e505ad409cdcc200f8558e10fe2865d
Dimitrius <tosavilovi@mail.ru>
2021-05-03 22:13:10 +0300

    11

--- testApp/urls.py
+++ testApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'1/$', views.index_html, name="index"),
-    url(r'contacts/$', views.contacts, name="contacts")
+    url(r'contacts/$', views.contacts, name="contact")
     
 ]
