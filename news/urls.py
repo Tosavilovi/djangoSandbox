8da6a82c241681f32a4fe939bc9b00cc7df2e383
Dimitrius <tosavilovi@mail.ru>
2021-05-03 22:33:06 +0300

    add model for news

---
+++ news/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'1/$', views.index_html, name="index"),
+    url(r'contacts/$', views.contacts, name="contact")
+    
+]
